8460748a7088ca0aa30af485d20922911ea59f0a
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-07-31 22:57:57 +0530

    Removed twilio bug

--- server/utilities/messager.js
+++ server/utilities/messager.js
@@ -8,13 +8,13 @@ const client = twilio(accountSid, authToken);
 
 const sendOTP = (user) => {
   // console.log(user);
-  client.messages
-  .create({
-     body: user.otp+'This is the OTP from Stackoverflow clone',
-     from: '+14175053515',
-     to: user.phno
-   })
-    .then(message => console.log(message.sid))
+  client.verify
+  .services(process.env.TWILI0_SERVICE)
+  .verifications.create({
+    to: user.phno,
+    channel: 'sms',
+  })
+  // .then(message => console.log(message.sid))
     .then((verification) => {
       console.log('OTP Send SUccess');
       return true;
